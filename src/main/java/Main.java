a1ae775cfd8583b583c35a25702bd501c5b3577d
Ben-21 <bs@schaefer-inet.de>
2023-06-03 10:26:25 +0200

    implemented method squareNumber

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,18 @@
+
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please insert the number you want to square: ");
+        int inputNumber = scanner.nextInt();
+
+        Calculations squareNumber = new Calculations();
+        int result = squareNumber.squareNumber(inputNumber);
+        System.out.println(inputNumber + " --> " + result);
+
+    }
 }
 
-//fsghfghdfgh
+
